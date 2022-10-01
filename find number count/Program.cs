6bd864f286707f1a64a6c3d6417405a5c9a6db36
Virvon <dan130205@gmail.com>
2022-10-01 19:37:21 +0300

    find number count

---
+++ find number count/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_number_count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int result = 0;
+            int minBorder = 100;
+            int maxBorder = 999;
+
+            Console.Write("Введи число из промежутка (1 <= N <= 27): ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i <= maxBorder; i += number)
+            {
+                if(i >= minBorder)
+                {
+                    result++;
+                }
+            }
+
+            Console.WriteLine("Количество трехзначных чисел, кратных " + number + ": " + result);
+            Console.ReadKey();
+        }
+    }
+}
